c5af5fbf001eeebb1650794f7bfc7cf819f3f915
Luis Alejo <lou.m.alejo@gmail.com>
2020-06-16 15:39:29 -0400

    wrap up day 2

--- App/Components/UserImage.js
+++ App/Components/UserImage.js
@@ -1,37 +1,48 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import Sizes from '../Helpers/Sizes'
+import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
+import Sizes from '../Helpers/Sizes';
+
+const image = require('../../assets/user.png');
 
 const UserImage = () => {
-  return (
-    <View style={styles.container}>
-  
-    </View>
-  );
-}
+	return (
+		<View style={styles.outerContainer}>
+      <View style={styles.innerContainer}>
+			  <Image style={styles.image} source={image} />
+      </View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    height: Sizes.userImageSides,
-    width: Sizes.userImageSides,
-    backgroundColor: '#222',
-    alignItems: 'center',
+	outerContainer: {
+		height: Sizes.userImageSides,
+		width: Sizes.userImageSides,
+		backgroundColor: '#e33',
+		alignItems: 'center',
     justifyContent: 'center',
-    borderColor: '#bbb',
-    borderWidth: 4,
-    shadowColor: "#000",
-    shadowOffset: { 
-      width: 9, 
-      height: 9 
-    }, 
-    shadowOpacity: 0.50,
-    shadowRadius: 12.35,
-    elevation: 19, 
-    transform: [{ rotate: '45deg'}], 
-  }
+		borderColor: '#bbb',
+		borderWidth: 4,
+		shadowColor: '#000',
+		shadowOffset: {
+      width: 9,
+			height: 9
+		},
+		shadowOpacity: 0.5,
+		shadowRadius: 12.35,
+		elevation: 19,
+		transform: [ { rotate: '45deg' } ]
+  },
+  innerContainer: {
+    height: Sizes.userImageSides - 10,
+    width: Sizes.userImageSides - 10,
+    overflow: 'hidden'
+  },
+	image: {
+		width: Sizes.userImageSides + 10,
+		height: Sizes.userImageSides + 10,
+		transform: [ { rotate: '-45deg' } ],
+	}
 });
 
-
-
 export default UserImage;
-
