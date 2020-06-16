b7a11cfb86d293779b04398286b721e14508e564
WabaScript <rjwahba@gmail.com>
2020-06-16 09:37:20 -0400

    test

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions, TouchableOpacity } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Whats up!</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text> Click Me!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,4 +21,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    borderRadius: 30,
+    width: windowWidth / 8,
+    backgroundColor: "seagreen",
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
